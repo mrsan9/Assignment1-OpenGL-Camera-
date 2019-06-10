94e998b03ce54717da3e1c414434c2f4c430dd85
Sandeep Salimeda <sandeepsalimeda@gmail.com>
2019-06-10 23:57:53 +0530

    Fixed errors

--- CameraAssignment1/Source.cpp
+++ CameraAssignment1/Source.cpp
@@ -68,7 +68,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Camera Opengl", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
